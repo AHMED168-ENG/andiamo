243f8f3573b343773ed976ffc3fdf17059843b0b
ahmed zaky <aalsaeed@nadeer.com.sa>
2024-02-26 21:07:45 +0200

    updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 
 
 module.exports = {
+    // reactStrictMode: true,
+    // unoptimized : true,
+    // swcMinify:true,
     reactStrictMode: true,
+    output: 'export',
     images: {
-        domains: ['stackfood.6am.one'],
+        // domains: ['stackfood.6am.one'],
        // staticPageGenerationTimeout: 1500//Domain of image host
+       unoptimized: true,
     },
 }
